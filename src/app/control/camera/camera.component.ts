796584731aaee2aeeef0b43e6b9506417d4824e5
ivanofffff <ivan.cocusse@epfedu.fr>
2025-03-28 12:04:28 +0100

    changement du style avec implementation de bootstrap sur le comp Camera et changement de la dispostion de la page control

--- src/app/control/camera/camera.component.ts
+++ src/app/control/camera/camera.component.ts
@@ -1,53 +1,51 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
-import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.component.html',
-  standalone: true,
-  imports: [
-    NgIf
-  ],
+  standalone : true,
+  imports: [NgIf],
   styleUrls: ['./camera.component.scss']
 })
 export class CameraComponent implements OnInit, OnDestroy {
   @ViewChild('videoElement', { static: true }) videoElement!: ElementRef<HTMLVideoElement>;
-
+  
   private mediaStream: MediaStream | null = null;
   isCameraActive = false;
   errorMessage: string | null = null;
 
   constructor() {}
 
-  ngOnInit() {
-    // Vous pouvez démarrer la caméra automatiquement si souhaité
-    // this.startCamera();
-  }
+  ngOnInit() {}
 
   async startCamera() {
     try {
-      console.log('Tentative d\'accès à la caméra');
-
-      // Lister les périphériques disponibles
       const devices = await navigator.mediaDevices.enumerateDevices();
-      console.log('Périphériques disponibles:', devices);
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+      if (videoDevices.length === 0) {
+        this.errorMessage = 'Aucune caméra détectée';
+        return;
+      }
 
       this.mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: {
+        video: { 
+          deviceId: videoDevices[0].deviceId ? 
+            { exact: videoDevices[0].deviceId } : undefined,
           width: { ideal: 1280 },
           height: { ideal: 720 }
         }
       });
 
-      console.log('Flux média obtenu');
-
       if (this.videoElement) {
-        this.videoElement.nativeElement.srcObject = this.mediaStream;
+        const videoEl = this.videoElement.nativeElement;
+        videoEl.srcObject = this.mediaStream;
 
-        // Écouteur pour vérifier quand la vidéo commence
-        this.videoElement.nativeElement.onloadedmetadata = () => {
-          console.log('Métadonnées chargées');
-          this.videoElement.nativeElement.play();
+        videoEl.onloadedmetadata = () => {
+          videoEl.play().catch(e => {
+            console.error('Erreur lors de la lecture:', e);
+          });
         };
 
         this.isCameraActive = true;
@@ -62,21 +60,19 @@ export class CameraComponent implements OnInit, OnDestroy {
 
   stopCamera() {
     if (this.mediaStream) {
-      // Arrêter tous les tracks de la caméra
       this.mediaStream.getTracks().forEach(track => track.stop());
-
-      // Réinitialiser le flux vidéo
+      
       if (this.videoElement) {
-        this.videoElement.nativeElement.srcObject = null;
+        const videoEl = this.videoElement.nativeElement;
+        videoEl.srcObject = null;
       }
-
+      
       this.mediaStream = null;
       this.isCameraActive = false;
     }
   }
 
   ngOnDestroy() {
-    // S'assurer que la caméra est arrêtée quand le composant est détruit
     this.stopCamera();
   }
 }
