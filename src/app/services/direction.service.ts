707a764158bd731fc269b79a30792f100b0e8cc1
icocusse <ivan.cocusse@epfedu.fr>
2025-03-31 16:04:29 +0200

    test pour dir

--- src/app/services/direction.service.ts
+++ src/app/services/direction.service.ts
@@ -14,7 +14,7 @@ export class DirectionService {
   constructor(private http: HttpClient) {
     // Pour le déploiement, vous pourriez vouloir configurer ceci dans votre environment.ts
     // Ceci est un exemple, vous devrez adapter selon votre structure
-    this.apiUrl = environment.apiUrl || 'http://192.168.1.39:5000/direction';
+    this.apiUrl = 'https://aecc-85-190-234-40.ngrok-free.app/direction';
   }
 
   // Options HTTP complètes
